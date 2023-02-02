1160cb7de655896498f6e554b2ce9f52a9e99cca
rida-aman <124303297+rida-aman@users.noreply.github.com>
2023-02-02 21:25:14 +0100

    ex1 task 31.01.2023

---
+++ ex1.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>My Projet PHP</title>
+</head>
+<body>
+  <main>
+    <?php
+      $name = "Rida Aman";
+      $groupuuid = "abc123";
+
+      echo "name : " . $name . "<br>";
+      echo "Groupuuid : " . $groupuuid .  "<br>";
+
+      $name = "David";
+      echo "Hello world! My name is \"" . $name . "\".".  "<br>";
+
+      echo "Today's date is: " . date("F j, Y") .  "<br>";
+
+      $title = "PHP is interesting";
+      echo "<h1>" . $title . "</h1>";
+
+    ?>
+
+      <table>
+      <tr>
+        <th>S.N.</th>
+        <th>Name</th>
+        <th>Grade</th>
+      </tr>
+      <?php
+        $g1 = 5;
+        $g2 = 4;
+        $g3 = 5;
+
+        $students = array(
+          array("John", $g1),
+          array("Jane", $g2),
+          array("Jim", $g3)
+        );
+
+        for ($i = 0; $i < count($students); $i++) {
+          echo "<tr>";
+          echo "<td>" . ($i + 1) . "</td>";
+          echo "<td>" . $students[$i][0] . "</td>";
+          echo "<td>" . $students[$i][1] . "</td>";
+          echo "</tr>";
+        }
+      ?>
+    </table>
+  </main>
+</body>
+</html>
